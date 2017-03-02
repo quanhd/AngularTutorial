fa1212a681c453c9e70c1a1f2d820a09a108d63e
quanhd <quanhd0103@gmail.com>
2017-03-02 15:54:29 -0800

    More on Components

--- APM - Start/app/app.module.ts
+++ APM - Start/app/app.module.ts
@@ -3,13 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent }  from './app.component';
 import { ProductListComponent } from './products/product-list.component';
-
+import { ProductFilterPipe } from './products/product-filter.pipe';
 @NgModule({
   imports: [ 
               BrowserModule,
-              FormsModule ],
+              FormsModule 
+           ],
   declarations: [ AppComponent,
-                  ProductListComponent],
+                  ProductListComponent,
+                  ProductFilterPipe
+                ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
